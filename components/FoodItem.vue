6611df57d5f8f77ec83e2c58f13c7edf0e95ac10
Romeo Albeza Jr. <rlalbezajr@student.apc.edu.ph>
2026-01-15 21:42:34 +0800

    Create FoodItem.vue

---
+++ components/FoodItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Santol and Mango',
+      message: 'I like santol and mango'
+    }
+  }
+};
+</script>
+
+<style></style>
